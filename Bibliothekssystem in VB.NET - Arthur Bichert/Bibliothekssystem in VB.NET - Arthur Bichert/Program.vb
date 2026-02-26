d6d1759b949d35134005060f03a52b54f57a1502
ArthurBichert <bichertarthur@gmail.com>
2026-02-26 11:23:21 +0100

    Programm angelegt
    Programm angelegt, testen ob Dateien über den Upload richtig comittet werden.

---
+++ Bibliothekssystem in VB.NET - Arthur Bichert/Bibliothekssystem in VB.NET - Arthur Bichert/Program.vb
@@ -0,0 +1,9 @@
+'Programmentwurf - Bibliothekssystem in VB.NET Teil 1
+
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("Hello World!")
+    End Sub
+End Module
