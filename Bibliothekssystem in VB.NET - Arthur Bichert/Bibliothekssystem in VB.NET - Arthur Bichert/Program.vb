4b1df3f80caa5fa46f053e7c2747e67f1cb70352
ArthurBichert <bichertarthur@gmail.com>
2026-02-26 11:24:32 +0100

    Delete Bibliothekssystem in VB.NET - Arthur Bichert/Bibliothekssystem in VB.NET - Arthur Bichert directory
    Nicht korrekt ausgeführt, Ordner wird gelöscht

--- Bibliothekssystem in VB.NET - Arthur Bichert/Bibliothekssystem in VB.NET - Arthur Bichert/Program.vb
+++
@@ -1,9 +0,0 @@
-'Programmentwurf - Bibliothekssystem in VB.NET Teil 1
-
-Imports System
-
-Module Program
-    Sub Main(args As String())
-        Console.WriteLine("Hello World!")
-    End Sub
-End Module
